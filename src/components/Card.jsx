4cfa3b5db05ede7f84f90b1398926685564c6c74
Brandon Lyons <brandon_lyons98@yahoo.com>
2024-01-23 14:50:22 -0500

    Add Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,39 @@
-import classes from './Card.module.css';
+import classes from '../modules/Card.module.css';
+import { decrementQuantity, incrementQuantity } from './HelperFunctions';
+import { formatCurrency } from '../utilities/CurrencyFormatter';
+import products from '../utilities/FetchProducts';
+import { useState } from 'react';
 
-async function fetchProducts() {
-  let response = await fetch('https://fakestoreapi.com/products');
-  let data = await response.json();
-  return data;
-}
-
-const products = await fetchProducts();
-
-function Card() {
-  console.log('here', products[0].title);
+function Card({id, title, price, imgUrl, quantity}) {
+  const [itemQuantity, setItemQuantity] = useState(0);
+  quantity = itemQuantity;
+  console.log(products);
 
   return (
     <>
       <div className={classes.container}>
-        {products.map((item) => (
-          <div key={item.id} className={classes.cardStyles}>
+        <div key={id} className={classes.cardStyles}>
+          <div className={classes.card}>
             <div>
-              <img src={item.image}/>
-              <h2>{item.title}</h2>
-              <h3>${item.price}</h3>
+              <img src={imgUrl}/>
+              <h2>{title}</h2>
             </div>
+            {itemQuantity === 0 ? (
+              <div>
+                <h3 className={classes.closedPrice}>{formatCurrency(price)}</h3>
+                <button className={classes.addBtn} onClick={() => {setItemQuantity(incrementQuantity(id-1))}}>+ Add To Cart</button>
+              </div>
+            ) : <div>
+                  <h3>{formatCurrency(price)}</h3>
+                  <div className={classes.counter}>
+                    <button id={classes.counterBtn} onClick={() => {setItemQuantity(decrementQuantity(id-1))}}>-</button>
+                    <p>{quantity}</p>
+                    <button id={classes.counterBtn} onClick={() => {setItemQuantity(incrementQuantity(id-1))}}>+</button>
+                  </div>
+                </div>
+                }
           </div>
-        ))}
+        </div>
       </div>
     </>
   )
