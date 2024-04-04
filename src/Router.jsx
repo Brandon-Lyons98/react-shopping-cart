b29136d7e885cfefb329cf43e40b00df6e13e1ff
Brandon Lyons <brandon_lyons98@yahoo.com>
2024-04-04 13:03:32 -0500

    Add React router paths

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DisplayShop from "./components/Shop";
+import ShoppingCart from "./components/Cart";
 import App from "./App";
-// import Homepage from "./pages/Homepage";
-import DisplayShop from "./pages/Shop";
-import ShoppingCart from "./pages/Cart";
+import ErrorPage from "./components/ErrorPage";
+import { CartProvider } from "./components/CartContext";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <App />,
+      errorElement: <ErrorPage />
     },
     {
       path: '/shop',
@@ -20,7 +22,11 @@ const Router = () => {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  )
 }
 
 export default Router;
