fbb6493261f8ede64d5dc6e29f3e691725929d6d
Brandon Lyons <brandon_lyons98@yahoo.com>
2024-04-04 12:34:14 -0500

    Add shop page logic

---
+++ src/components/Shop.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Items from "./Items";
+
+function DisplayShop() {
+  return (
+    <div>
+      <Navbar />
+      <Items />
+    </div>
+  )
+}
+
+export default DisplayShop;
